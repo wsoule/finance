f51aa83873c02c4812afe8570d0e439155ee205f
Wyat Soule <71346930+wsoule@users.noreply.github.com>
2023-06-09 12:11:14 -0400

    Feature/wrapper component (#1)
    * create wrapper component - need to resolve toast bug
    * remove comments on wrapper.module.scss

---
+++ libs/react/src/components/wrapper/wrapper.tsx
@@ -0,0 +1,38 @@
+import { Box } from '@chakra-ui/layout';
+import { FC, PropsWithChildren } from 'react';
+
+export type WrapperPropsSize = 'small' | 'medium';
+
+export interface WrapperProps extends PropsWithChildren{
+  size?: WrapperPropsSize;
+}
+
+export const Wrapper: FC<WrapperProps> = ({ children, size }) => {
+  return (
+    <Box
+      marginX='auto'
+      marginY='1rem'
+      maxWidth={`${wrapperSizeToPixels(size)}px`}
+      width='100%'
+    >
+      {children}
+    </Box>
+  );
+};
+
+export function wrapperSizeToPixels(size: WrapperPropsSize = 'medium'): number {
+  switch (size) {
+    case 'medium': {
+      return 800;
+    }
+    case 'small': {
+      return 400;
+    }
+    default: {
+      const neverSize: never = size;
+      throw new Error(`Invalid size '${neverSize}' provided to Wrapper.sizeToPixels`);
+    }
+  }
+}
+
+export default Wrapper;
